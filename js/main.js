f55aa7114d04cc9d9ff8acc5c06942a0df9453d3
Johan Forsberg <johan@slentrian.org>
2012-09-30 13:48:23 +0200

    some reformatting

--- js/main.js
+++ js/main.js
@@ -5,52 +5,58 @@ $(function () {
     // Tools
     var tools = OldPaint.tools = new OldPaint.Tools();
     var Tool = OldPaint.Tool;
-    tools.add(new Tool({name: "pencil", key: "p", preview: true,
-                        draw: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            layer.draw_line(stroke.last, stroke.pos,
-                                            stroke.brush, stroke.color);}}));
+    tools.add(new Tool(
+        {name: "pencil", key: "p", preview: true,
+         draw: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             layer.draw_line(stroke.last, stroke.pos,
+                             stroke.brush, stroke.color);}}));
 
-    tools.add(new Tool({name: "points", preview: true,
-                        draw: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            layer.draw_brush(stroke.pos, stroke.brush, stroke.color);
-                        }}));
+    tools.add(new Tool(
+        {name: "points", preview: true,
+         draw: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             layer.draw_brush(stroke.pos, stroke.brush, stroke.color);
+         }}));
 
-    tools.add(new Tool({name: "line", preview: true,
-                        draw: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            layer.restore_backup(layer.last_change);
-                            layer.draw_line(stroke.start, stroke.pos,
-                                            stroke.brush, stroke.color);
-                        }}));
+    tools.add(new Tool(
+        {name: "line", preview: true,
+         draw: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             layer.restore_backup(layer.last_change);
+             layer.draw_line(stroke.start, stroke.pos,
+                             stroke.brush, stroke.color);
+         }}));
 
-    tools.add(new Tool({name: "rectangle", preview: true,
-                        draw: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            var size = {x: stroke.pos.x - stroke.start.x,
-                                        y: stroke.pos.y - stroke.start.y};
-                            layer.restore_backup(layer.last_change);
-                            layer.draw_rectangle(stroke.start, size,
-                                                 stroke.brush, stroke.color,
-                                                 stroke.shift);
-                        }, help: "Hold SHIFT key to make a filled rectangle."}));
+    tools.add(new Tool(
+        {name: "rectangle", preview: true,
+         draw: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             var size = {x: stroke.pos.x - stroke.start.x,
+                         y: stroke.pos.y - stroke.start.y};
+             layer.restore_backup(layer.last_change);
+             layer.draw_rectangle(stroke.start, size,
+                                  stroke.brush, stroke.color,
+                                  stroke.shift);
+         }, help: "Hold SHIFT key to make a filled rectangle."}));
 
-    tools.add(new Tool({name: "ellipse", preview: true,
-                        draw: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            var radius = {x: stroke.pos.x - stroke.start.x,
-                                          y: stroke.pos.y - stroke.start.y};
-                            layer.restore_backup(layer.last_change);
-                            layer.draw_ellipse(stroke.start, radius,
-                                               stroke.brush, stroke.color,
-                                               stroke.shift);
-                        }, help: "Hold SHIFT key to make a filled ellipse."}));
+    tools.add(new Tool(
+        {name: "ellipse", preview: true,
+         draw: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             var radius = {x: stroke.pos.x - stroke.start.x,
+                           y: stroke.pos.y - stroke.start.y};
+             layer.restore_backup(layer.last_change);
+             layer.draw_ellipse(stroke.start, radius,
+                                stroke.brush, stroke.color,
+                                stroke.shift);
+         }, help: "Hold SHIFT key to make a filled ellipse."}));
 
-    tools.add(new Tool({name: "floodfill", oneshot: true, preview: false,
-                        draw: function (drawing, stroke) {
-                            drawing.layers.active.draw_fill(stroke.pos, stroke.color);
-                        }}));
+    tools.add(new Tool(
+        {name: "floodfill", oneshot: true, preview: false,
+         draw: function (drawing, stroke) {
+             drawing.layers.active.draw_fill(stroke.pos, stroke.color);
+         }}));
 
     // tools.add(new Tool({name: "gradientfill",
     //                     draw: function (drawing, stroke) {
@@ -58,47 +64,49 @@ $(function () {
     //                             stroke.pos, drawing.palette.range);
     //                     }}));
 
-    tools.add(new Tool({name: "brush", preview: false,
-                        help: "Drag the corner handles to resize selection, and click anywhere else to finish.",
-                        before: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            var rect = Util.rectify(stroke.start,
-                                                    {x: stroke.pos.x + 1,
-                                                     y: stroke.pos.y + 1});
-                            var select_brush = function () {
-                                console.log("action");
-                                var layer = drawing.layers.active;
-                                var brush = new OldPaint.ImageBrush({
-                                    patch: layer.make_patch(drawing.selection),
-                                    image_type: drawing.image_type});
-                                user_brushes.add(brush);
-                                user_brushes.set_active(brush);
-                                drawing.set_selection();
-                                tools.set_active(tools.previous);
-                            };
-                            drawing.set_selection(rect, select_brush);
-                        },
-                        draw: function (drawing, stroke) {
-                            var layer = drawing.layers.active;
-                            var rect = Util.rectify(stroke.start,
-                                                    {x: stroke.pos.x + 1,
-                                                     y: stroke.pos.y + 1});
-                            drawing.set_selection(rect);
-                        },
-                        after:function (drawing, stroke) {
-                            drawing.trigger("selection_done");
-                        }}));
+    tools.add(new Tool(
+        {name: "brush", preview: false,
+         help: "Click and drag to select the area you want to copy.",
+         before: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             var rect = Util.rectify(stroke.start,
+                                     {x: stroke.pos.x + 1,
+                                      y: stroke.pos.y + 1});
+             var select_brush = function () {
+                 console.log("action");
+                 var layer = drawing.layers.active;
+                 var brush = new OldPaint.ImageBrush({
+                     patch: layer.make_patch(drawing.selection),
+                     image_type: drawing.image_type});
+                 user_brushes.add(brush);
+                 user_brushes.set_active(brush);
+                 drawing.set_selection();
+                 tools.set_active(tools.previous);
+             };
+             drawing.set_selection(rect, select_brush);
+         },
+         draw: function (drawing, stroke) {
+             var layer = drawing.layers.active;
+             var rect = Util.rectify(stroke.start,
+                                     {x: stroke.pos.x + 1,
+                                      y: stroke.pos.y + 1});
+             drawing.set_selection(rect);
+         },
+         after:function (drawing, stroke) {
+             drawing.trigger("selection_done");
+         }}));
 
-    tools.add(new Tool({name: "picker", preview: false,
-                        draw: function (drawing, stroke) {
-                            var rgb, color=0;
-                            drawing.layers.each(function (layer) {
-                                color = layer.get_pixel(stroke.pos);
-                                if (color[0] != undefined) color = color[0];
-                            });
-                            if (color != drawing.palette.foreground)
-                                drawing.palette.set_foreground(color);
-                        }}));
+    tools.add(new Tool(
+        {name: "picker", preview: false,
+         draw: function (drawing, stroke) {
+             var rgb, color=0;
+             drawing.layers.each(function (layer) {
+                 color = layer.get_pixel(stroke.pos);
+                 if (color[0] != undefined) color = color[0];
+             });
+             if (color != drawing.palette.foreground)
+                 drawing.palette.set_foreground(color);
+         }}));
 
     var tools_view = new OldPaint.ToolsView({collection: tools});
     tools.set_active(tools.at(0));
