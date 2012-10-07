9afd6e1f46f7362b0ce51c3b9021babf17330dea
Johan Forsberg <johan.forsberg@gmail.com>
2012-10-07 07:05:52 +0200

    add missing files for the menu

---
+++ js/menu.js
@@ -0,0 +1,63 @@
+(function( $ ){
+  $.fn.linearMenu = function(items, context) {
+      if ($(this.children()[0]).hasClass("menu")) {
+          return;
+      }
+      var el = this, backup = this.html(), wrap = $('<div id="menu">');
+      var history = $('<div class="history">');
+      wrap.append(history);
+      var menu = $('<div class="items">');
+      wrap.append(menu);
+      el.html(wrap);
+      console.log("menu");
+
+      function find_shortcut (string) {
+          for(var i=0; i<string.length; i++) {
+              if (string[i] != " " &&
+                  string[i] == string[i].toUpperCase()) return string[i].toLowerCase();
+          }
+          return null;
+      }
+
+      function markup_letter (string, index) {
+          console.log(string, index);
+          var before = string.substr(0, index),
+              after = string.substr(index+1);
+          return before + "<span class='shortcut'>" + string[index] + "</span>" + after;
+      }
+
+      function close_menu () {
+          $(document).unbind("keydown");  // remove all the menu keybindings
+          $(document).unbind("keyup");
+          el.html(backup);
+      }
+      $(document).bind("keyup.Esc", close_menu);
+
+      function show_menu (items) {
+          var tmp = $('<div>');
+          $.each(items, function (name, item) {
+              var btn = $('<button class="menuitem">');
+              var action = function (event) {
+                  btn.removeClass("menuitem");
+                  history.append(btn);
+                  $(document).unbind("keydown");  // remove all the menu keybindings
+                  if (_.isFunction(item)) {
+                      close_menu();
+                      _.bind(item, context)();
+                  }
+                  else show_menu(item);
+                  return false;  // prevents any normal keybindings from firing..?
+              };
+              var shortcut = find_shortcut(name);
+              $(document).bind("keydown." + shortcut, action);
+              btn.click(action);
+              btn.html(markup_letter(name, name.indexOf(shortcut.toUpperCase())));
+              //btn.text(name);
+              tmp.append(btn);
+          });
+          menu.html(tmp);
+          //el.children().show("slide", { direction: "down" }, 100);
+      }
+      show_menu(items);
+  };
+})( jQuery );
